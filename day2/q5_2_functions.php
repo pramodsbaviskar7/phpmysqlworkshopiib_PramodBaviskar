75745be3162707c01759991a64f0caf635568c35
pramodsbaviskar7 <pramodsbaviskar7@gmail.com>
2020-09-30 20:25:41 +0530

    second commit after changing file names day-1 and day-2

---
+++ day2/q5_2_functions.php
@@ -0,0 +1,18 @@
+<?php  
+
+function myName($name){
+
+	echo "My name is ".$name;
+}
+myName("Ravichandran<br>");
+function writeName2($firstname){
+
+	echo "$firstname";
+}
+echo "My name is ";
+writeName2("Jai");
+echo " Sharma<br>";
+echo "My brother's name is ";
+writeName2("Ram");
+echo " Sharma<br>";
+?>
